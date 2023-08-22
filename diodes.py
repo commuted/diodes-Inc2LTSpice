dacc7c19cb09f6878ba0ab356c30071f5f14b435
Ronald Hinchley <commuted@gmail.com>
2023-08-21 18:24:47 -0700

    Update diodes.py
    Something to do with Windoze and utf-8

--- diodes.py
+++ diodes.py
@@ -1,4 +1,3 @@
-import os
 import re
 
 '''
@@ -10,8 +9,7 @@ Read Diodes Inc file: Transistor (BJT) Master Table.spice.txt
 Add Vceo, Icrating iand mfg=diodesinc which are scraped from the 
 file: Transistor (BJT) Master Tablr.xlsx
 Remove spurious line gaps in + notation:w.
-Remove spurious subckt ends
-'''
+Remove spurious subckt ends'''
 
 
 # Scrape master CVS doc to get Vceo and Icrating parameters for parametric selection
@@ -28,13 +26,14 @@ flag = True
 with open('out.txt','w', encoding='utf-8-sig') as f:
     with open("master.spice.txt", "r+") as fp:
       for line in fp:
-        x = line.strip().split()
+        line = line.strip()
+        x = line.split()
         if x: # something on line
           # remove purious subckt ends
           if x[0].strip().lower() == '.ends': #process subckt ends
             if flag == True: # remove purious subckt ends
               print("removing spurious ends",line)
-              line = os.linesep
+              line = ''
             else:# close subckt
               flag = True
           if (x[0].strip().lower() =='.subckt') and (flag == True): # process start of subckt
@@ -45,13 +44,12 @@ with open('out.txt','w', encoding='utf-8-sig') as f:
               if br: # test for sometimes '()' brackets
                 vce = csv[x[1]][3].strip()[0:-1]
                 im  = str(float(csv[x[1]][4].strip()[0:-1]) )
-                line = line[0:br.span()[0]] + " mfg=DiodesInc " + "Vceo="+vce  + " Icrating="+im +")"+os.linesep
+                line = line[0:br.span()[0]] + " mfg=DiodesInc " + "Vceo="+vce  + " Icrating="+im +")"
               else:
                 vce = csv[x[1]][3].strip()[0:-1]
                 im  = str(float(csv[x[1]][4].strip()[0:-1]) )
                 line = line.strip()
                 line = (line + " mfg=DiodesInc " + "Vceo="+vce  + " Icrating="+im)
-                line = line +   os.linesep
-        if x: # remove cr
-          f.writelines(line)
-
+          if line:   
+            
+            f.writelines(line+'\n')
